1f0b3e6d6dc49da708c2af1d9897085abbe97023
TurtleLions <92956863+TurtleLions@users.noreply.github.com>
2023-12-18 11:29:40 -0800

    angle/velocity

--- game.js
+++ game.js
@@ -2,9 +2,12 @@
 
 var dotX = 0;
 var dotY = 0;
+var angle = 0;
+var velocity = 0;
 var visibleCanvas = null;
 var context = null;
 var dotImage = null;
+var TO_RADIANS = Math.PI/180; 
 
 document.addEventListener("DOMContentLoaded", function(event) {
     visibleCanvas = document.getElementById('game');
@@ -16,24 +19,33 @@ document.addEventListener("DOMContentLoaded", function(event) {
     dotImage.onload = function() {
         context.drawImage(dotImage, dotX, dotY);
     }
+    
 });
 
 function move(event) {
     switch (event.key) {
         case "ArrowUp":
-            dotY -= 10;
+            velocity += 1;
             break;
         case "ArrowDown":
-            dotY += 10;
+            velocity -= 1;
             break;
         case "ArrowLeft":
-            dotX -= 10;
+            angle -= 1;
             break;
         case "ArrowRight":
-            dotX += 10;
+            angle += 1;
             break;
     }
     context.clearRect(0, 0, visibleCanvas.width, visibleCanvas.height);
+    rotateAndPaintImage(context, dotImage, angle*TO_RADIANS, dotX, dotY, dotX, dotY
     context.drawImage(dotImage, dotX, dotY);
     event.preventDefault(); // prevents arrows from scrolling
+}
+function rotateAndPaintImage ( context, image, angleInRad , positionX, positionY, axisX, axisY ) {
+  context.translate(positionX, positionY);
+  context.rotate(angleInRad);
+  context.drawImage(image, -axisX, -axisY);
+  context.rotate(-angleInRad);
+  context.translate(-positionX, -positionY);
 }
