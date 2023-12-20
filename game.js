300ae936fb40422713038f48a5bab1893a431816
TurtleLions <92956863+TurtleLions@users.noreply.github.com>
2023-12-20 12:35:45 -0800

    Update game.js

--- game.js
+++ game.js
@@ -1,13 +1,16 @@
 // Copyright (c) 2020 eContriver LLC
 
-var dotX = 500;
-var dotY = 200;
+var intDotX = 200;
+var intDotY = 200;
+var dotMag = 0;
 var angle = 0;
 var velocity = 0;
 var visibleCanvas = null;
 var context = null;
 var dotImage = null;
 var TO_RADIANS = Math.PI/180; 
+
+
 
 document.addEventListener("DOMContentLoaded", function(event) {
     visibleCanvas = document.getElementById('game');
@@ -17,25 +20,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
     dotImage = new Image();
     dotImage.src = 'dot.png';
     dotImage.onload = function() {
-        context.drawImage(dotImage, dotX, dotY);
+        context.drawImage(dotImage, intDotX + (dotMag*Math.cos(angle*TO_RADIANS)), intDotY + (dotMag*Math.sin(angle*TO_RADIANS)));
     }
-    while(True){
-        dotX+=Math.round(velocity*Math.sin(angle*TO_RADIANS))
-        dotY+=Math.round(velocity*Math.cos(angle*TO_RADIANS))
-        if(velocity>0){
-            velocity-=1;
-        }
-        dotX+=1;
-    }
+    // while(True){
+    //     dotX+=Math.round(velocity*Math.sin(angle*TO_RADIANS))
+    //     dotY+=Math.round(velocity*Math.cos(angle*TO_RADIANS))
+    //     if(velocity>0){
+    //         velocity-=1;
+    //     }
+    //     dotX+=1;
+    // }
 });
 
 function move(event) {
     switch (event.key) {
         case "ArrowUp":
-            velocity += 1;
+            dotMag += 1;
             break;
         case "ArrowDown":
-            velocity -= 1;
+            dotMag -= 1;
             break;
         case "ArrowLeft":
             angle -= 1;
@@ -45,13 +48,5 @@ function move(event) {
             break;
     }
     context.clearRect(0, 0, visibleCanvas.width, visibleCanvas.height);
-    rotateAndPaintImage(context, dotImage, angle*TO_RADIANS, dotX, dotY, 300, 300);
     event.preventDefault(); // prevents arrows from scrolling
-}
-function rotateAndPaintImage ( context, image, angleInRad , positionX, positionY, axisX, axisY ) {
-  context.translate(positionX, positionY);
-  context.rotate(angleInRad);
-  context.drawImage(image, -axisX, -axisY);
-  context.rotate(-angleInRad);
-  context.translate(-positionX, -positionY);
 }
